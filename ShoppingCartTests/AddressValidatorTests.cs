5cbcc38ecccf4cfbfcbe598f10d4018877860503
feepigab <144051096+feepigab@users.noreply.github.com>
2023-12-20 15:04:09 -0700

    address validator tests refactor

--- ShoppingCartTests/AddressValidatorTests.cs
+++ ShoppingCartTests/AddressValidatorTests.cs
@@ -9,79 +9,44 @@ namespace BusinessLogicTests
         [Fact]
         public void Valid_address()
         {
-            // Arrange
             var address = new Address
             {
                 Country = "USA",
                 City = "Phoenix",
                 Street = "123 Main Ave"
             };
-
             var addressValidator = new AddressValidator();
 
-            // Act
             bool isValid = addressValidator.IsValid(address);
 
-            // Assert
             Assert.True(isValid);
         }
 
-        [Fact]
-        public void Invalid_address_no_country()
+        [InlineData("", "City", "Street")]
+        [InlineData("Country", "", "Street")]
+        [InlineData("Country", "City", "")]
+        [InlineData(null, null, null)]
+        [Theory]
+        public void Address_is_Invalid(string country, string city, string street)
         {
-            var address = new Address
-            {
-                Country = string.Empty,
-                City = "Phoenix",
-                Street = "123 Main Ave"
-            };
-            var addressValidator = new AddressValidator();
+            var address = GenerateAddress(country, city, street);
 
-            bool isValid = addressValidator.IsValid(address);
+            var validator = new AddressValidator();
+            bool isValid = validator.IsValid(address);
 
             Assert.False(isValid);
         }
 
-        [Fact]
-        public void Invalid_address_no_city()
+        private Address GenerateAddress(string country, string city, string street)
         {
-            var address = new Address
+            if (country == null || city == null || street == null) { return null; }
+
+            return new Address
             {
-                Country = "USA",
-                City = string.Empty,
-                Street = "123 Main Ave"
+                Country = country,
+                City = city,
+                Street = street
             };
-            var addressValidator = new AddressValidator();
-
-            bool isValid = addressValidator.IsValid(address);
-
-            Assert.False(isValid);
-        }
-
-        [Fact]
-        public void Invalid_address_no_street()
-        {
-            var address = new Address
-            {
-                Country = "USA",
-                City = "Phoenix",
-                Street = string.Empty
-            };
-            var addressValidator = new AddressValidator();
-
-            bool isValid = addressValidator.IsValid(address);
-
-            Assert.False(isValid);
-        }
-
-        [Fact]
-        public void Invalid_address_null()
-        {
-            var addressValidator = new AddressValidator();
-
-            bool isValid = addressValidator.IsValid(null);
-
-            Assert.False(isValid);
         }
     }
 }
