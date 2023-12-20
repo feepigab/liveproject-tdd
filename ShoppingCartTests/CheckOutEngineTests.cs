4dc1101eeb4a3e4b5adc5c2ed645b93050198335
feepigab <144051096+feepigab@users.noreply.github.com>
2023-12-20 15:27:17 -0700

    checkout engine tests refactor

--- ShoppingCartTests/CheckOutEngineTests.cs
+++ ShoppingCartTests/CheckOutEngineTests.cs
@@ -17,48 +17,33 @@ namespace BusinessLogicTests
             _mockMapper = new Mock<IMapper>();
         }
 
-        [Fact]
-        public void Few_items_no_discount()
+        [InlineData(0, 2, CustomerType.Standard, 1)]
+        [InlineData(0, 4, CustomerType.Standard, 3)]
+        [InlineData(10, 1.8, CustomerType.Premium, 1)]
+        [InlineData(10, 3.6, CustomerType.Premium, 3)]
+        [Theory]
+        public void Checkout_Totals(double expectedDiscount, double expectedTotal, CustomerType customerType, uint itemQuantity)
         {
             var cart = new Cart
             {
-                Items = new List<Item> {
-                    new Item {
+                Items = new List<Item>
+                {
+                    new Item
+                    {
                         Price = 1.0,
-                        Quantity = 3
+                        Quantity = itemQuantity
                     }
                 },
-                CustomerType = CustomerType.Standard
+                CustomerType = customerType
             };
 
             var engine = new CheckOutEngine(_mockShippingCalculator.Object, _mockMapper.Object);
             _mockShippingCalculator.Setup(x => x.CalculateShippingCost(cart)).Returns(1.0);
+
             var result = engine.CalculateTotals(cart);
 
-            Assert.Equal(0, result.CustomerDiscount);
-            Assert.Equal(4, result.Total);
-        }
-
-        [Fact]
-        public void One_item_premium_discount()
-        {
-            var cart = new Cart
-            {
-                Items = new List<Item> {
-                    new Item {
-                        Price = 1.0,
-                        Quantity = 1
-                    }
-                },
-                CustomerType = CustomerType.Premium
-            };
-
-            var engine = new CheckOutEngine(_mockShippingCalculator.Object, _mockMapper.Object);
-            _mockShippingCalculator.Setup(x => x.CalculateShippingCost(cart)).Returns(1.0);
-            var result = engine.CalculateTotals(cart);
-
-            Assert.Equal(10.0, result.CustomerDiscount);
-            Assert.Equal(1.8, result.Total); // 10% discount
+            Assert.Equal(expectedDiscount, result.CustomerDiscount);
+            Assert.Equal(expectedTotal, result.Total);
         }
     }
 }
